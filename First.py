3274512b5e41deff77fbf542e64bda036929acdc
Oleg <olegpetrovspb@yandex.ru>
2020-04-20 12:47:22 +0300

    add First.py and update .gitignore

--- First.py
+++ First.py
@@ -1,16 +1,28 @@
-print('Введите 3 числа:')
-a = int(input())
-b = int(input())
-c = int(input())
+from statistics import mean
 
-sum1 = int(a + b + c)
-composition = int(a*b*c)
-arithmeticmean = float((a+b+c)/2)
+array = list(map(int, input('Введите 3 числа: ').split()))
+# map - функция, применяющая другую функцию (название которой указывается в первом аргументе) к набору данных
 
-print('Сумма:', sum1)
+s = sum(array)
+composition = array[0] * array[1] * array[2]
+arithmetic_mean = s / len(array)
+arithmetic_mean1 = mean(array)
+
+print('Сумма:', s)
+print('Сумма: ' + str(s))  # так не надо, код читается хуже
 print('Произведение:', composition)
-print('Среднее арифметическое:', arithmeticmean)
+print('Среднее арифметическое:', arithmetic_mean)
+print('Среднее арифметическое:', arithmetic_mean1)
 
+'''
+line = input('Введите 3 числа:')  # ввод данных одной строкой
+print(line)
+array = line.split()
+print(array)
 
+for i in range(len(array)):  # преобразование элементов массива к числам
+    array[i] = int(array[i])
 
-
+print(array)
+print(sum(array))
+'''
