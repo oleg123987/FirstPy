65179b8b9a1038eda2acff8970266607b26878c5
Oleg <olegpetrovspb@yandex.ru>
2020-04-20 12:09:37 +0300

    Initial commit

---
+++ First.py
@@ -0,0 +1,16 @@
+print('Введите 3 числа:')
+a = int(input())
+b = int(input())
+c = int(input())
+
+sum1 = int(a + b + c)
+composition = int(a*b*c)
+arithmeticmean = float((a+b+c)/2)
+
+print('Сумма:', sum1)
+print('Произведение:', composition)
+print('Среднее арифметическое:', arithmeticmean)
+
+
+
+
