5cd03b41ac93c4c825f0cd897591ea0b9e7f2038
Oleg <olegpetrovspb@yandex.ru>
2020-04-30 12:53:24 +0300

    Решена задача 3_2

--- HW_3_2.py
+++ HW_3_2.py
@@ -1,16 +1,18 @@
+import string
+
 # Посчитать общее количество слов в файле
+ru = ['в', 'а', 'но']
+en = ['a', 'the', 'at', 'and', 'of']
 
-
-f = open(r'C:\Users\Oleg\PycharmProjects\First\resources\sentence.txt', encoding="utf-8")
-# text = f.readline().lower()  # TODO помнить, что в файле может быть несколько строк и нужно прочитать их всех
+f = open(r'C:\Users\Oleg\PycharmProjects\First\resources\sentence.txt', encoding='utf-8')
 words_amount = 0
-line = f.readline()
-while line:
-    words_amount += len(line.split())
-    line = f.readline()
-print(words_amount)
+for line in f.readlines():  # построчное чтение из файла
+    for word in line.split():  # цикл по словам в строке
+        # из слова удаляем лишние сиволы (не буквы), приводим к нижнему регистру, учитываем случай для "-" внутри слова
+        word = word.strip(string.punctuation + string.whitespace).lower().replace('-', '')
+        # если слово состоит из букв и не содержится в исключениях (массивы ru, en)
+        if word.isalpha() and word not in ru + en:
+            words_amount += 1
 f.close()
-
-
-
+print('Sum of words:', words_amount)
 
