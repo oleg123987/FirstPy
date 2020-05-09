a70619ef56659512c21bbb775b98365fb33d6e3e
Alexandra Guseva <flutesa@ya.ru>
2020-05-09 18:35:23 +0200

    Использование библиотеки pathlib для возможности работать с файлами в win/nix системах

--- HW_3_1_vowels.py
+++ HW_3_1_vowels.py
@@ -1,14 +1,22 @@
 import string
+from pathlib import Path
 
 # Посчитать в предложениях количество гласных букв
 ru = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я']
 en = ['a', 'i', 'u', 'e', 'o', 'y']
 
-f = open(r'C:\Users\Oleg\PycharmProjects\First\resources\sentence.txt', encoding='utf-8')
+# f = open(r'C:\Users\Oleg\PycharmProjects\First\resources\sentence.txt', encoding='utf-8')
+f = open(Path(Path.cwd() / 'resources' / 'sentence.txt', encoding='utf-8'))
+'''
+Где почитать о pathlib:
+https://habr.com/ru/post/453862
+https://python-scripts.com/pathlib
+'''
+
 odd_letters = 0
 for line in f.readlines():  # построчное чтение из файла
     for word in line.split():  # цикл по словам в строке
-        # из слова удаляем лишние сиволы (не буквы), приводим к нижнему регистру, учитываем случай для "-" внутри слова
+        # из слова удаляем лишние символы (не буквы), приводим к нижнему регистру, учитываем случай для "-" внутри слова
         word = word.strip(string.punctuation + string.whitespace).lower().replace('-', '')
         # если слово состоит из букв и не содержится в исключениях (массивы ru, en)
         for c in word:
@@ -22,18 +30,3 @@ for line in f.readlines():  # построчное чтение из файла
 
 f.close()
 print('Количество гласных eng и ru всех строчек в файле:', odd_letters)
-
-
-
-
-# TODO добавить все возможные гласные буквы русского алфавита в массив ru
-
-
-
-# TODO добавить все возможные гласные буквы английского алфавита в массив en
-
-
-
-# TODO реализовать подсчёт всех гласных букв через цикл по элементам массива ru
-
-# TODO считать из файла вторую строку и реализовать ту же самую логику, только для массива en
