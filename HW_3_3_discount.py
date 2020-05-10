b418ab1d1917e43a01cd39496d74bdf4d1518efb
Oleg <olegpetrovspb@yandex.ru>
2020-05-11 00:00:51 +0300

    HW_3_3_discount задача решена с подключением csv файла

--- HW_3_3_discount.py
+++ HW_3_3_discount.py
@@ -1,8 +1,21 @@
 # Если сумма товаров в корзине пользователя больше или равна 3000, то применить скидку 30%.
 # В ответ вывести итоговую сумму.
 
-goods = ['1000', '20', '310', '2300']
+# goods = ['1000', '20', '310', '2300']
+goods_csv = []
 
+f = open(r'C:\Users\Oleg\PycharmProjects\First\resources\goods.csv', encoding='utf-8')
+for line in f.readlines():  # построчное чтение из файла
+    print(line)
+    goods_csv.append(line)
+        # .replace("\n", "")
+
+    # line = line.lower()
+    # for letter in line:
+print(goods_csv)
+goods_csv = [line.rstrip() for line in goods_csv]
+print(goods_csv)
+f.close()
 
 def price_goods(prices):
     total = sum(map(int, prices))
@@ -16,7 +29,7 @@ def price_goods(prices):
 
 # s = sum(int(goods[i]) for i in range(0, int(len(goods))))
 
-price_goods(input().split())
-# res = price_goods(goods)
+# price_goods(input().split())
+res = price_goods(goods_csv)
 # print(res)
 # print(price_goods(['1000', '20', '310']))
