9fd61924af0c93d0fc915a1c98db41849c6e0d27
Oleg <olegpetrovspb@yandex.ru>
2020-05-11 12:52:43 +0300

    Улучшение кода

--- HW_3_3_discount.py
+++ HW_3_3_discount.py
@@ -1,21 +1,6 @@
 # Если сумма товаров в корзине пользователя больше или равна 3000, то применить скидку 30%.
 # В ответ вывести итоговую сумму.
 
-# goods = ['1000', '20', '310', '2300']
-goods_csv = []
-
-f = open(r'C:\Users\Oleg\PycharmProjects\First\resources\goods.csv', encoding='utf-8')
-for line in f.readlines():  # построчное чтение из файла
-    print(line)
-    goods_csv.append(line)
-        # .replace("\n", "")
-
-    # line = line.lower()
-    # for letter in line:
-print(goods_csv)
-goods_csv = [line.rstrip() for line in goods_csv]
-print(goods_csv)
-f.close()
 
 def price_goods(prices):
     total = sum(map(int, prices))
@@ -27,9 +12,17 @@ def price_goods(prices):
     return total
 
 
-# s = sum(int(goods[i]) for i in range(0, int(len(goods))))
+f = open(r'C:\Users\Oleg\PycharmProjects\First\resources\goods.csv', encoding='utf-8')
 
-# price_goods(input().split())
+goods_csv = [line.rstrip() for line in f.readlines()]  # построчное чтение из файла
+f.close()
+print(goods_csv)
 res = price_goods(goods_csv)
+
+
+
+# s = sum(int(goods[i]) for i in range(0, int(len(goods))))
+# goods = ['1000', '20', '310', '2300']
+# price_goods(input().split())
 # print(res)
 # print(price_goods(['1000', '20', '310']))
