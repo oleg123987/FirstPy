f1c9d44d3be75a1bd0b8ba6d0e6c51c0bc4e3715
Oleg <olegpetrovspb@yandex.ru>
2020-05-11 12:34:07 +0300

    Запись в файл, комментарии в коде

--- CW_3_parse_reqs.py
+++ CW_3_parse_reqs.py
@@ -1,14 +1,17 @@
 
 file = open('resources/requirements.txt')
 
-unique = set()  # создаем "множества" set, видимо присваиваем ему новый объект unique, скорее всего что-то из ООП
+unique = set()  # создаем пустое "множество" unique, присваиваем в unique пустой set
 all = []  # создаем пустой массив all
 for line in file.readlines():  # читаем файл по строкам
-    value = line.strip()  # удаляем лишние символы и пробелы у строчки слева и справа
-    unique.add(value)  # возвращаем в функцию unique , проверяем уникальность через set() и добавляем в unique
-    all.append(value)  # добавляем все строчки
-
+    # line = line.strip()  # удаляем лишние символы и пробелы у строчки слева и справа
+    unique.add(line)  # добавляем в unique новое значение, проверяется уникальность через set()
+    all.append(line)  # добавляем строку в массив all
+file.close()
 print(len(unique))  # считаем кол-во строк в unique (уникальные)
 print(len(all))  # считаем кол-во всех строк
+print(''.join(unique))  # преобразуем unique массив в строчки с переносом
 
-print('\n'.join(unique))  # преобразуем unique массив в строчки с переносом
+file = open('resources/requirements_unique.txt', 'w+')
+file.writelines(unique)
+file.close()
