28a4f116b0159724012f652988f4f48ef50c4aa6
Oleg <olegpetrovspb@yandex.ru>
2020-05-20 22:30:28 +0300

    CW_4_logs_analyzer решение задачи HW_05_21_03_favorite_movies возможный вариант решения задачи

--- CW_4_logs_analyzer.py
+++ CW_4_logs_analyzer.py
@@ -1,16 +1,21 @@
 # в файлах statHrefs.log и statSearchHrefs.log выгружены логи с сервера приложения парсера веб-сайтов о недвижимости
 # Нужно посчитать, сколько всего парсер обошёл страниц
 
+def myfunc(x):
+    file = open(x)
+    summa = 0
+    amounts = []
+    for line in file.readlines():
+        summa += int(line.split()[8])
+        amounts.append(int(line.split()[8]))
+    file.close()
 
-file = open('resources/statHrefs.log')
-summa = 0
-amounts = []
-for line in file.readlines():
-    summa += int(line.split()[8])
-    amounts.append(int(line.split()[8]))
-file.close()
+    print(summa)
+    print(sum(amounts))
+    print(len(amounts))
+    print(amounts[:10])
 
-print(summa)
-print(sum(amounts))
-print(len(amounts))
-print(amounts[:10])
+
+(myfunc('resources/statHrefs.log'))
+print('Другой файл:')
+(myfunc('resources/statSearchHrefs.log'))
