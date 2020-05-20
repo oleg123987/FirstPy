153679b240b52e23135a107a81c6aab2eb396769
Oleg <olegpetrovspb@yandex.ru>
2020-05-20 20:02:10 +0300

    init CW_4 logs

--- CW_4_logs_analyzer.py
+++ CW_4_logs_analyzer.py
@@ -1,2 +1,16 @@
 # в файлах statHrefs.log и statSearchHrefs.log выгружены логи с сервера приложения парсера веб-сайтов о недвижимости
 # Нужно посчитать, сколько всего парсер обошёл страниц
+
+
+file = open('resources/statHrefs.log')
+summa = 0
+amounts = []
+for line in file.readlines():
+    summa += int(line.split()[8])
+    amounts.append(int(line.split()[8]))
+file.close()
+
+print(summa)
+print(sum(amounts))
+print(len(amounts))
+print(amounts[:10])
